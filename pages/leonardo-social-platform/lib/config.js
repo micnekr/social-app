11e1c1926f03c416d5fd2df326834da50dc0e1a6
micnekr <misha.nekrasov@gmail.com>
2021-04-02 13:44:41 +0100

    added basic nginx

--- pages/leonardo-social-platform/lib/config.js
+++ pages/leonardo-social-platform/lib/config.js
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV !== "production";
 
-// export const server = dev ? "http://localhost:3000" : "https://server.com";
+// export const server = dev ? "http://localhost:3000" : "https://name.com";
 export const server = dev ? "http://localhost:3000" : "http://localhost:3000";
